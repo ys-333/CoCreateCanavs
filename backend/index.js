acf16c02915e8d82b05daf1b713e1209eb7cbef8
ys-333 <yashwantsing333@gmail.com>
2023-10-17 02:39:49 +0530

    socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log('setting the projec') ;
+const express = require('express')
+const app = express()
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  //   res.statusCode(200)
+  res.end('<h1>Hey how are you doing,Har Har Mahadev</h1>')
+})
+
+server.listen(3000, () => {
+  console.log('Listening on server 3000')
+})
