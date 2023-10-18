ce39fd24ad8dbe64b76dafafed12df3669af0a2b
ys-333 <yashwantsing333@gmail.com>
2023-10-18 23:20:32 +0530

    socket.io setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
+const http = require('node:http')
+const { createServer } = http
+const { Server } = require('socket.io')
 
-const http = require('http')
+const server = createServer(app)
+const io = new Server(server)
 
-const server = http.createServer((req, res) => {
-  //   res.statusCode(200)
-  res.end('<h1>Hey how are you doing,Har Har Mahadev</h1>')
+io.on('connection', (socket) => {
+  socket.on('chat', (msg) => {
+    console.log('message', msg)
+    io.emit('chat', 'hello how are you doing')
+  })
 })
 
 server.listen(3000, () => {
-  console.log('Listening on server 3000')
+  console.log('listening on portal 3000')
 })
