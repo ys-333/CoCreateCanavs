4729a339693c283512af0c01a790d4c460e456c7
ys-333 <yashwantsing333@gmail.com>
2023-10-31 01:54:30 +0530

    canvas added

---
+++ frontend/src/components/Whiteboard/index.jsx
@@ -0,0 +1,5 @@
+const WhiteBoard = () => {
+  return <canvas className="border board-dark border-3 h-100 w-100"></canvas>
+}
+
+export default WhiteBoard
--- frontend/src/pages/Room/index.jsx
+++ frontend/src/pages/Room/index.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './index.css'
 
+import WhiteBoard from '../../components/Whiteboard'
+
 const RoomPage = () => {
   const [tool, setTool] = useState('pencil')
   const [color, setColor] = useState('black')
 
-  console.log('hello world and save');
+  console.log('hello world and save')
 
   return (
     <div className="row">
-      <h1 className="text-center py-5">White Board Sharing App</h1>
-      <div className="col-md-8 px-5 mt-4  mt-5 d-flex align-items-center justify-content-between ">
+      <h1 className="text-center py-4">
+        White Board Sharing App{' '}
+        <span className="text-primary">[Uses Online: 0]</span>
+      </h1>
+      <div className="col-md-10 px-5 mt-4  mt-5 d-flex align-items-center justify-content-between ">
         <div className="d-flex col-md-2 justify-content-between gap-1">
           <div className="d-flex gap-1 align-items-center ">
             <label htmlFor="pencil">Pencil</label>
@@ -19,6 +24,7 @@ const RoomPage = () => {
               type="radio"
               name="tool"
               value="pencil"
+              checked={tool === 'pencil'}
               onChange={(e) => {
                 setTool(e.target.value)
               }}
@@ -32,6 +38,7 @@ const RoomPage = () => {
               type="radio"
               name="tool"
               value="line"
+              checked={tool === 'line'}
               onChange={(e) => {
                 setTool(e.target.value)
               }}
@@ -45,6 +52,7 @@ const RoomPage = () => {
               type="radio"
               name="tool"
               value="rectangle"
+              checked={tool === 'rectangle'}
               onChange={(e) => {
                 setTool(e.target.value)
               }}
@@ -53,17 +61,27 @@ const RoomPage = () => {
             />
           </div>
         </div>
-        <div className="col-md-7">
+        <div className="col-md-2 mx-auto">
           <label htmlFor="color">Select color</label>
           <input
             type="color"
             name=""
             id="color"
-            className="mt-1"
+            className="mt-1 ms-3"
             value={color}
             onChange={(e) => setColor(e.target.value)}
           />
         </div>
+        <div className="col-md-3 d-flex gap-2">
+          <button className="btn btn-primary mt-1">Undo</button>
+          <button className="btn btn-outline-primary mt-1">Redo</button>
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-danger">Clear Canvas</button>
+        </div>
+      </div>
+      <div className="col-md-10 border mt-4 canvas-box">
+        <WhiteBoard />
       </div>
     </div>
   )
